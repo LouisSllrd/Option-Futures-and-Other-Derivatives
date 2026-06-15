19f44e7b1f7a2904c334e36a65fe57aae754f6da
LouisSllrd <l310sallard@gmail.com>
2026-06-15 19:27:49 +0200

    update future definition and add new asset commodity

--- src/chapters/chap3/futures.py
+++ src/chapters/chap3/futures.py
@@ -4,17 +4,25 @@ import pandas as pd
 
 
 class Asset(ABC):
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str = "default_asset") -> None:
         self.name = name
 
 
-class Future(Asset):
+class Commodity(Asset):
+    def __init__(self, name: str, spot_price: float) -> None:
+        super().__init__(name)
+        self.spot_price = spot_price
+
+
+class Future:
     def __init__(
         self,
+        underlying: Asset,
         contract_price: float,
         maturity: pd.Timestamp,
-        underlying: Asset,
+        contract_size: int = 1000,
     ) -> None:
+        self.underlying = underlying
         self.contract_price = contract_price
         self.maturity = maturity
-        self.underlying = underlying
+        self.contract_size = contract_size
